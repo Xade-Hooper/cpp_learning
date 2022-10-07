7176c93b5f5c1f5c93f838bed59b49d8f5e95098
fedcalderon <fedcalderon@users.noreply.github.com>
2022-10-06 23:32:38 -0400

    added another line of code

--- start.cpp
+++ start.cpp
@@ -4,5 +4,10 @@ int main(void)
 {
     std::cout<<"yes, it's a beautiful world@" << std::endl;
 
+    int x = 8;
+    int y = 23;
+
+    std::cout << x << " + " << y << " = " << x*y << std::endl;
+
     return 0;
 }
